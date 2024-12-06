0f91a4bfa2712ffe1704f45d2f3cf1f685c34992
golgepapaz <hsolter@gmail.com>
2024-12-06 17:12:11 +0300

    ggg

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,20 +1,24 @@
+use std::str::SplitAsciiWhitespace;
+
 use helpers;
 fn part1() {
     let tt = helpers::read_file("input", 2);
     tt.lines().for_each(|line|{
-        let t: Vec<_> = line.split(' ').map(|s| s.parse::<i32>().unwrap()).collect();
-        t.as_slice().windows(2).filter(|x| x[0] < x[1]);
+        line.split_ascii_whitespace().map(|s| s.parse::<u8>().unwrap()).collect::<Vec<_>>().filter(|x| predicate(x)).count();
+    //     let t: Vec<_> = line.split(' ').map(|s| s.parse::<i32>().unwrap()).collect();
+    //    t.iter().filter(|x| x[0] < x[1]);
     }
 )
 }
 fn predicate(values: &[i32])-> bool {
     let mut inc =true;
     let mut dec = true;
-    let valid =true;
 
-    values.windows(2).filter(|&x| {
+
+    for x in values.windows(2)
+    {
        if x[0].abs_diff(x[1]) > 3 {
-        return false;
+        return false
        }
        inc &= x[0] < x[1];
        dec &= x[0] > x[1];
@@ -22,7 +26,7 @@ fn predicate(values: &[i32])-> bool {
            return false;
        }
     }
-    )
-
+  
+  true
 }
     
